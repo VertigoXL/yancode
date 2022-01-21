b01080dff4c24e99d849a6362cf04731f2699e85
Vertigo <97815847+VertigoXL@users.noreply.github.com>
2022-01-20 21:14:58 -0600

    Inital commit

---
+++ ifelse.py
@@ -0,0 +1,16 @@
+# Just a mockup of a type of spag code (spaghetti code),
+# I call it Yandere Code. It's code that is just filled with if statements
+# this is a joke btw. Enjoy
+
+import time
+
+whileTrue = True
+x = 0
+
+while whileTrue:
+    print('''if poopoo = peepee:\n  print('poopoo')''')
+    x = x + 21
+
+while whileTrue:
+    if x == 42:
+        print('ur mum gay')
